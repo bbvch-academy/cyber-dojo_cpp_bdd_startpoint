04ec3ac650428df1c244564611048babe4925f5d
Michel Estermann <estermann.michel@gmail.com>
2018-09-08 18:32:51 +0200

    Moved hiker_steps.cpp and stop cyber_dojo script when make fails.

--- cucumber-cpp/start_point/cyber-dojo.sh
+++ cucumber-cpp/start_point/cyber-dojo.sh
@@ -1,5 +1,6 @@
 LANG=c
-make --always-make 2>&1
-# Test output can be formatted as progress or documentation
-sleep 1
-cucumber -f progress . -r .
+if  make --always-make 2>&1 ; then
+  # Test output can be formatted as progress or documentation
+  sleep 1
+  cucumber -f progress . -r .
+fi
